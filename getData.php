38671c61311018a78f1d7c0927c204d7ae3b4c3f
csanchezra <44853224+csanchezra@users.noreply.github.com>
2018-11-08 15:24:48 -0600

    Create getData.php

---
+++ getData.php
@@ -0,0 +1,23 @@
+<?php
+
+exec('php /var/www/html/carpeta_de_proyecto/disco.php');
+
+// MySQL database connection code
+$connection = mysqli_connect("localhost","usuariomysql","contrasena","reporte_nick") or die("Error " . mysqli_error($connection));
+
+
+$sql = "SELECT * FROM raiz";
+$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+//create an array
+$array = array();
+$i = 0;
+while($row = mysqli_fetch_assoc($result))
+{
+    $tipo = $row['tipo'];
+    $espacio = $row['espacio'];
+    $array['cols'][] = array('type' => 'string');
+    $array['rows'][] = array('c' => array( array('v'=> $tipo), array('v'=>(int)$espacio)) );
+}
+$data = json_encode($array);
+echo $data;
+?>
